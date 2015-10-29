a4a0c000b6fbe6b68dc560b1bd9f81ff89e11db0
michaelb1 <TrixieThe1TrueDog!>
2015-10-29 18:19:15 -0400

    base program works. ToDo build exceptions and pre and post conditions.

--- src/com/SWE510/Demo3.java
+++ src/com/SWE510/Demo3.java
@@ -1,54 +1,62 @@
 package com.SWE510;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-/**Shows a constructor passing information about constructor failure to an exception handler. Define class SomeClass,
- *  which throws an exception in the constructor. Your program should try to create an object of type SomeClass and
- *  catch the exception that is thrown from the constructor.
+
+/**
+ * Shows a constructor passing information about constructor failure to an exception handler. Define class SomeClass,
+ * which throws an exception in the constructor. Your program should try to create an object of type SomeClass and
+ * catch the exception that is thrown from the constructor.
  * Created by mb on 10/29/15.
  */
 public class Demo3
 	{
-	public static void main (String[] args)
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args)
 		{
-		//Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
 		SomeClass validObject = new SomeClass("The Goldfinch", "9780316055444", 775, "It used to be a perfectly " +
 				                                                                             "ordinary day but now it sticks up on the calendar like a rusty nail.");
 
-		System.out.print(validObject);
 		boolean goLoop = true;
-		//create invalid object and catch the exeption it throws from the constructor.
+//a lot to interate thoough collecting user input to contruct a new object and catch exceptions.
 		do {
+			System.out.print("Please enter your favorite book. e.g. " + validObject.getBookName() + "\n");
+			String bName = scanner.nextLine();
+			System.out.printf("\n");
+
+			System.out.print("Please enter the ISBN. e.g. " + validObject.getIsbn() + "\n");
+			String isbn = scanner.nextLine();
+
+			System.out.print("Please enter your favorite quote from the book. \ne.g. " + validObject.getQuote() +
+					                 "\n");
+			String quote = scanner.nextLine();
+
+
+			System.out.print("Please enter the number of pages in " + "\"" + bName + "\"" + ".\n");
+			int bLength = scanner.nextInt();
+
+			//pass variables to constructor. Catch exception thrown by constructor.
 			try {
-
-				SomeClass invalidObject = new SomeClass("We, the drowned", "0", 0, "Nobody sailed into a raging " +
-						                                                                     "gale with all" +
-						                                                           " sails " +
-						                                                          "set. You didn't confront a storm head-on."
-						                                                          + " You adjusted the sails and found a balance.");
+				SomeClass userObject = new SomeClass(bName, isbn, bLength, quote);
+				System.out.print("Your book :\n" + userObject);
 				goLoop = false;
-				System.out.print(invalidObject);
 
 			}
+
 			catch (InputMismatchException inputMismatchException) {
-				System.out.println(inputMismatchException);
+				System.out.println("A book has to have more than 0 pages.");
+
 			}
+
+
 		}
 		while (goLoop);
-
-
-
-		//SomeClass[] allBooks = new SomeClass{validObject, invalidObject};
-		/*SomeClass userObject = new SomeClass();
-
-		System.out.print("Please enter your favorite book. e.g. " + validObject.getBookName() + "\n");
-		userObject.setBookName(scanner.next());
-		System.out.print("Please enter the ISBN. e.g. " + validObject.getIsbn() + "\n");
-		userObject.setIsbn(scanner.next());
-		System.out.print("Please enter the number of pages in " + userObject.getBookName() + "\n");
-		userObject.setBookLength(scanner.nextInt());
-		System.out.print("Please enter your favorite quote from the book. e.g. " + validObject.getQuote() + "\n");
-		userObject.setQuote(scanner.next());*/
 
 
 		}
--- src/com/SWE510/SomeClass.java
+++ src/com/SWE510/SomeClass.java
@@ -1,58 +1,47 @@
 package com.SWE510;
 
-import java.awt.print.Book;
 import java.util.InputMismatchException;
 
-/**pre- and post-conditions are mandatory for all classes but Demo3
+/**
+ * pre- and post-conditions are mandatory for all classes but Demo3
  * Shows a constructor passing information about constructor failure to an exception handler. Define class SomeClass,
- *  which throws an exception in the constructor. Your program should try to create an object of type SomeClass and
- *  catch the exception that is thrown from the constructor.
+ * which throws an exception in the constructor. Your program should try to create an object of type SomeClass and
+ * catch the exception that is thrown from the constructor.
  * Created by mb on 10/29/15.
  */
 public class SomeClass
 	{
 
-	/*each method will have a pre-condition
-			e.g. depositMoney() method would presumably money should be positive.
-			 So, a precondition would be money > 0.
-			 */
 	private String bookName;
 	private String isbn;
 	private int bookLength;
 	private BookQuote quote;
 
 
-	// constructor
+	/**
+	 * constructor Pre-condition arguments passed in  must be the correct type. Booklenth must be greater than 0.
+	 *
+	 * @param bookName   the book name
+	 * @param isbn       the isbn
+	 * @param bookLength the book length
+	 * @param quote      the quote
+	 * @throws InputMismatchException the input mismatch exception
+	 */
 	public SomeClass(String bookName, String isbn, int bookLength, String quote) throws InputMismatchException
 
 		{
-		if (bookLength <= 0) throw new InputMismatchException("A book has to have pages");
-		if (bookName.length() == 0) throw new IllegalArgumentException("A book with no name?");
+		if (bookLength <= 0) {
+			throw new InputMismatchException();
+		}
 
 		this.bookName = bookName;
 		this.isbn = isbn;
 		this.bookLength = bookLength;
 		this.quote = new BookQuote(quote);
 		}
-
-		/*insert  code passing information about constructor failure to an exception handler.
-		Throw an exception. Catch the exception that is thrown from the constructor which
-
-if bookLength is less than 1 throw an exception.
-		try {
-			bookLength >= 1;
-		} catch (FileNotFoundException e) {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(configFile));
-			writer.write(defaultConfig);
-			writer.close();
-			fis = new FileInputStream(file);
-		}
-
-		try{
-			builder = builderFactory.newDocumentBuilder();
-			document = builder.parse (fis);
-
-		}*/
+	/**constructor post-condition: Method will provide a new object of type SomeClass that holds variables with
+	 * getter and setter methods.
+*/
 
 	@Override
 	public String toString()
@@ -61,44 +50,83 @@ if bookLength is less than 1 throw an exception.
 		                     bookName, isbn, bookLength,quote);
 		}
 
+	/**
+	 * Gets book name.
+	 *
+	 * @return the book name
+	 */
 	public String getBookName()
 		{
 		return bookName;
 		}
 
 
-
+	/**
+	 * Gets isbn.
+	 *
+	 * @return the isbn
+	 */
 	public String getIsbn()
 		{
 		return isbn;
 		}
 
 
+	/**
+	 * Gets book length.
+	 *
+	 * @return the book length
+	 */
 	public int getBookLength()
 		{
 		return bookLength;
 		}
 
+	/**
+	 * Sets book length.
+	 *
+	 * @param bookLength the book length
+	 */
 	public void setBookLength(int bookLength)
 		{
 		this.bookLength = bookLength;
 		}
 
+	/**
+	 * Gets quote.
+	 *
+	 * @return the quote
+	 */
 	public BookQuote getQuote()
 		{
 		return this.quote;
 		}
 
+	/**
+	 * Sets book name.
+	 *
+	 * @param bookName the book name
+	 */
 	public void setBookName(String bookName)
 		{
 		this.bookName = bookName;
 		}
 
+	/**
+	 * Sets isbn.
+	 *
+	 * @param isbn the isbn
+	 */
 	public void setIsbn(String isbn)
 		{
 		this.isbn = isbn;
 		}
 
+	/**
+	 * Sets quote.
+	 *
+	 * @param quote the quote
+	 */
 	public void setQuote(BookQuote quote)
 		{
 		this.quote = quote;
